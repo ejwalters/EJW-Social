c18de9dcbfd6b37daff51435b8e66d5737fe64e1
Eric Walters <ericwalters@Erics-MBP.fios-router.home>
2017-02-26 09:35:47 -0500

    Sign in ui custom classes complete

---
+++ EJW-Social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  EJW-Social
+//
+//  Created by Eric Walters on 2/26/17.
+//  Copyright © 2017 Eric Walters. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
