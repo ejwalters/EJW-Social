275e534ae167edee7c8c593e40b9ca94e02805aa
Eric Walters <ericwalters@Erics-MBP.fios-router.home>
2017-03-01 15:15:29 -0500

    Added feed ui

--- EJW-Social/CircleView.swift
+++ EJW-Social/CircleView.swift
@@ -1,25 +1,25 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  EJW-Social
 //
-//  Created by Eric Walters on 2/26/17.
+//  Created by Eric Walters on 3/1/17.
 //  Copyright © 2017 Eric Walters. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
-        
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
 }
