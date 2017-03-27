0e76b334e66bed5b2620db2badc807cc6e3e209c
Eric Walters <ericwalters@erics-mbp.attlocal.net>
2017-03-27 19:38:09 -0400

    Completed Firebase storage download

--- EJW-Social/PostCell.swift
+++ EJW-Social/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
 
@@ -25,11 +26,30 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLabel.text = "\(post.likes)"
-    }
+        
+        if img != nil {
+            self.postImage.image = img
+        } else {
+                let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+                ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                    if error != nil {
+                        print("ERIC: unable to download image from firebase storage")
+                    } else {
+                        print("ERIC: image downloaded from firebase storage")
+                        if let imgData = data {
+                            if let img = UIImage(data: imgData) {
+                                self.postImage.image = img
+                                FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                            }
+                        }
+                    }
+                })
+            }
+        }
 
 
 }
