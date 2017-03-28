753b4c51e1518f1d4021d8e15a425a4c5a980e6f
Eric Walters <ericwalters@erics-mbp.attlocal.net>
2017-03-27 20:15:28 -0400

    Firebase image upload

--- EJW-Social/FeedVC.swift
+++ EJW-Social/FeedVC.swift
@@ -12,12 +12,14 @@ import SwiftKeychainWrapper
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var captionField: FancyField!
     @IBOutlet weak var imageAdd: CircleView!
     @IBOutlet weak var tableView: UITableView!
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected = false
 
     
     
@@ -93,10 +95,37 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
+            imageSelected = true
         } else {
             print("ERIC: A valid image was not selected")
         }
         imagePicker.dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func postButtonTapped(_ sender: Any) {
+        guard let caption = captionField.text, caption != "" else {
+            print("ERIC: Caption must be entered")
+            return
+        }
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("ERIC: An Image must be selected")
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            
+            let imgUid = NSUUID().uuidString
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData,metadata: metadata) { (metadata, error) in
+                if error != nil {
+                    print("ERIC: Unable to upload image to firebase storage")
+                } else {
+                    print("ERIC: Succesfully uploaded image to firebase storage")
+                    let downloadURL = metadata?.downloadURL()?.absoluteString
+                }
+            }
+        }
+    }
 }
